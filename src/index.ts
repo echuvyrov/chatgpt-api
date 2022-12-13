3c0f4fa552ea20b4d6ae9a2ed5d0b0cb0242d8c1
eugene.chuvyrov <eugene.chuvyrov@servicenow.com>
2022-12-13 14:04:15 -0800

    API

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ export * from './chatgpt-conversation'
 export * from './types'
 export * from './utils'
 export * from './openai-auth'
+
+import { Request, Response } from "express";
+
+export default async (req: Request, res: Response) => {
+  res.json({ message: "Hello guys. Welcome to Vercel" });
+};
+
